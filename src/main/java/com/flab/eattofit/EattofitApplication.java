0f78a40af4a856c3b12d57269257fe4087f3b1fe
devholic22 <hyunjoon.tech@gmail.com>
2024-08-06 23:42:10 +0900

    chore: 개발 기본 템플릿 코드 작성
    - 스프링 부트 코드 다운로드 및 적용
    - gitignore 설정 작성

---
+++ src/main/java/com/flab/eattofit/EattofitApplication.java
@@ -0,0 +1,13 @@
+package com.flab.eattofit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EattofitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EattofitApplication.class, args);
+	}
+
+}
