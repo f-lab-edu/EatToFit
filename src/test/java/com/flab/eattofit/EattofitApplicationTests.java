2a543345f622040643d14e0ee48771b257dc7bf7
Hyunjoon Choi <hyunjoon.tech@gmail.com>
2024-08-10 20:33:10 +0900

    chore: 개발 기본 템플릿 코드 작성 (#2)
    - 스프링 부트 코드 다운로드 및 적용
    - gitignore 설정 작성

---
+++ src/test/java/com/flab/eattofit/EattofitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.eattofit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EattofitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
