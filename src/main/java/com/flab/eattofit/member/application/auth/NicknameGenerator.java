3a9a35465db13e9979484e3a72ba06c5503ea04f
devholic22 <hyunjoon.tech@gmail.com>
2024-08-24 21:17:44 +0900

    feat: NicknameGenerator 구현
    - 닉네임 중복 시 닉네임을 새롭게 만들어주기 위한 NicknameGenerator 구현

---
+++ src/main/java/com/flab/eattofit/member/application/auth/NicknameGenerator.java
@@ -0,0 +1,6 @@
+package com.flab.eattofit.member.application.auth;
+
+public interface NicknameGenerator {
+
+    String generateNickname(String originName);
+}
---
+++ src/main/java/com/flab/eattofit/member/infrastructure/auth/RandomNicknameGenerator.java
@@ -0,0 +1,26 @@
+package com.flab.eattofit.member.infrastructure.auth;
+
+import com.flab.eattofit.member.application.auth.NicknameGenerator;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Component
+public class RandomNicknameGenerator implements NicknameGenerator {
+
+    private static final int RANDOM_LENGTH = 5;
+    private static final int DIGIT = 10;
+
+    @Override
+    public String generateNickname(final String originName) {
+        Random random = new Random();
+
+        String generated = IntStream.range(0, RANDOM_LENGTH)
+                .mapToObj(value -> String.valueOf(random.nextInt(DIGIT)))
+                .collect(Collectors.joining());
+
+        return originName + generated;
+    }
+}
