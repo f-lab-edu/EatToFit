cde235db65c32f7a48c139e855a603ef9a9b2512
devholic22 <hyunjoon.tech@gmail.com>
2024-08-24 16:43:22 +0900

    feat: RestTemplateConfig 추가
    - RestTemplate 설정을 하는 RestTemplateConfig 정의

---
+++ src/main/java/com/flab/eattofit/member/config/auth/RestTemplateConfig.java
@@ -0,0 +1,19 @@
+package com.flab.eattofit.member.config.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        DefaultUriBuilderFactory defaultUriBuilderFactory = new DefaultUriBuilderFactory();
+        defaultUriBuilderFactory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.NONE);
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setUriTemplateHandler(defaultUriBuilderFactory);
+        return restTemplate;
+    }
+}
