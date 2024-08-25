a25d7403d206b8a00797ebdbbcd9570f7d6c1c9d
devholic22 <hyunjoon.tech@gmail.com>
2024-08-25 16:22:09 +0900

    test: NicknameGenerator 테스트 작성
    - Member 단위 테스트에 NicknameGenerator 연결
    - RandomNicknameGenerator 테스트 작성

--- src/test/java/com/flab/eattofit/member/domain/member/MemberTest.java
+++ src/test/java/com/flab/eattofit/member/domain/member/MemberTest.java
@@ -1,5 +1,7 @@
 package com.flab.eattofit.member.domain.member;
 
+import com.flab.eattofit.member.infrastructure.member.FakeNicknameGenerator;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -9,6 +11,8 @@ import static org.assertj.core.api.SoftAssertions.assertSoftly;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SuppressWarnings("NonAsciiCharacters")
 class MemberTest {
+
+    private final NicknameGenerator nicknameGenerator = new FakeNicknameGenerator();
 
     @Test
     void 회원을_생성한다() {
@@ -23,6 +27,24 @@ class MemberTest {
         assertSoftly(softly -> {
             softly.assertThat(member.getEmail()).isEqualTo(email);
             softly.assertThat(member.getNickname()).isEqualTo(nickname);
+        });
+    }
+
+    @Test
+    void 회원은_회원가입_시_닉네임을_수정할_수_있다() {
+        // given
+        String email = "a@email.com";
+        String nickname = "hello";
+        String updatedName = nicknameGenerator.generateNickname(nickname);
+        Member member = Member.of(email, nickname);
+
+        // when
+        member.updateNicknameWithGenerator(nicknameGenerator);
+
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(member.getNickname()).isNotEqualTo(nickname);
+            softly.assertThat(member.getNickname()).isEqualTo(updatedName);
         });
     }
 }
