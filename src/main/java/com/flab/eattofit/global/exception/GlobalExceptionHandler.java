d94bcb6ef2c25ef51ba1b521854031fcac46322b
devholic22 <hyunjoon.tech@gmail.com>
2024-08-25 14:36:49 +0900

    fix: MethodArgumentNotValidException 메시지 감지 추가
    - MethodArgumentNotValidException 예외 처리 추가

--- src/main/java/com/flab/eattofit/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/flab/eattofit/global/exception/GlobalExceptionHandler.java
@@ -3,11 +3,24 @@ package com.flab.eattofit.global.exception;
 import com.flab.eattofit.global.exception.dto.ApiErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Objects;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final String METHOD_ARGUMENT_NOT_VALID = "METHOD_ARGUMENT_NOT_VALID";
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiErrorResponse> handleMethodArgumentNotValidException(final MethodArgumentNotValidException exception) {
+        BindingResult bindingResult = exception.getBindingResult();
+        String message = Objects.requireNonNull(bindingResult.getFieldError()).getDefaultMessage();
+        return getErrorMessageWithStatus(HttpStatus.BAD_REQUEST, METHOD_ARGUMENT_NOT_VALID, message);
+    }
 
     @ExceptionHandler(GlobalException.class)
     public ResponseEntity<ApiErrorResponse> handleGlobalException(final GlobalException exception) {
