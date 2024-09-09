4aa8d2148656596d981ffcdf49fd5fb0174bda24
devholic22 <devholic24@gmail.com>
2024-09-10 02:50:11 +0900

    feat: 회원 인터셉터 등록 및 글로벌 설정
    - 회원 인터셉터 최종 등록
    - 요청 경로 및 메서드에 따른 요청 구분하도록 클래스 작성

---
+++ src/main/java/com/flab/eattofit/global/config/interceptor/PathMatcherInterceptor.java
@@ -0,0 +1,51 @@
+package com.flab.eattofit.global.config.interceptor;
+
+import com.flab.eattofit.global.config.interceptor.support.HttpMethod;
+import com.flab.eattofit.global.config.interceptor.support.PathContainer;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class PathMatcherInterceptor implements HandlerInterceptor {
+
+    private final HandlerInterceptor handlerInterceptor;
+    private final PathContainer pathContainer;
+
+    public PathMatcherInterceptor(final HandlerInterceptor handlerInterceptor) {
+        this.handlerInterceptor = handlerInterceptor;
+        this.pathContainer = new PathContainer();
+    }
+
+    @Override
+    public boolean preHandle(final HttpServletRequest request,
+                             final HttpServletResponse response,
+                             final Object handler) throws Exception {
+        if (pathContainer.isNotIncludedPath(request.getServletPath(), request.getMethod())) {
+            return true;
+        }
+        return handlerInterceptor.preHandle(request, response, handler);
+    }
+
+    @Override
+    public void postHandle(final HttpServletRequest request,
+                           final HttpServletResponse response,
+                           final Object handler,
+                           final @Nullable ModelAndView modelAndView) throws Exception {
+
+        if (!pathContainer.isNotIncludedPath(request.getServletPath(), request.getMethod())) {
+            handlerInterceptor.postHandle(request, response, handler, modelAndView);
+        }
+    }
+
+    public PathMatcherInterceptor addPathPatterns(final String pathPattern, final HttpMethod... httpMethod) {
+        pathContainer.addIncludePatterns(pathPattern, httpMethod);
+        return this;
+    }
+
+    public PathMatcherInterceptor excludePathPattern(final String pathPattern, final HttpMethod... pathMethod) {
+        pathContainer.addExcludePatterns(pathPattern, pathMethod);
+        return this;
+    }
+}
