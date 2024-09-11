74e84a5b52544b36dce16f2de9d35688c84cb7d7
devholic22 <devholic24@gmail.com>
2024-09-11 20:29:26 +0900

    test: PathContainer 테스트 작성

---
+++ src/test/java/com/flab/eattofit/global/PathContainerTest.java
@@ -0,0 +1,50 @@
+package com.flab.eattofit.global;
+
+import com.flab.eattofit.global.config.interceptor.support.HttpMethod;
+import com.flab.eattofit.global.config.interceptor.support.PathContainer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class PathContainerTest {
+
+    private PathContainer pathContainer;
+
+    @BeforeEach
+    void setup() {
+        pathContainer = new PathContainer();
+    }
+
+    @Test
+    void include로_등록한_메서드와_uri가_같으면_false를_반환한다() {
+        // given
+        String uri = "/uri/test";
+        HttpMethod httpMethod = HttpMethod.GET;
+        pathContainer.addIncludePatterns(uri, httpMethod);
+
+        // when
+        boolean result = pathContainer.isNotIncludedPath(uri, httpMethod.name());
+
+        // then
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void exclude로_등록한_메서드와_uri가_같으면_true를_반환한다() {
+        // given
+        String uri = "/uri/test";
+        HttpMethod httpMethod = HttpMethod.GET;
+        pathContainer.addExcludePatterns(uri, httpMethod);
+
+        // when
+        boolean result = pathContainer.isNotIncludedPath(uri, httpMethod.name());
+
+        // then
+        assertThat(result).isTrue();
+    }
+}
