45e8629127285ec908b720b440ef7632f6fb35cf
devholic22 <devholic24@gmail.com>
2024-09-29 01:24:17 +0900

    feat: SportsRepository 정의
    - SportsRepository 리포지터리 정의
    - 리포지터리 구현체 정의

---
+++ src/main/java/com/flab/eattofit/exercise/infrastructure/sports/SportsRepositoryImpl.java
@@ -0,0 +1,23 @@
+package com.flab.eattofit.exercise.infrastructure.sports;
+
+import com.flab.eattofit.exercise.domain.sports.Sports;
+import com.flab.eattofit.exercise.domain.sports.SportsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class SportsRepositoryImpl implements SportsRepository {
+
+    private final SportsJpaRepository sportsJpaRepository;
+
+    @Override
+    public Sports save(final Sports sports) {
+        return sportsJpaRepository.save(sports);
+    }
+
+    @Override
+    public boolean existsByName(final String name) {
+        return sportsJpaRepository.existsByName(name);
+    }
+}
