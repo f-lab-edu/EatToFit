9b29d5d64a7b2987c2b3f1c101e7d5206ab5df20
devholic22 <devholic24@gmail.com>
2024-10-06 01:21:31 +0900

    test: ExerciseProfileController 통합 테스트 작성
    - ExerciseProfileController 통합 테스트 작성
    - API 문서화
    - 필요한 fixture 등록
    - API PostMapping 오류 수정
    - MockBean 등록

--- src/main/java/com/flab/eattofit/profile/ui/exerciseprofile/ExerciseProfileController.java
+++ src/main/java/com/flab/eattofit/profile/ui/exerciseprofile/ExerciseProfileController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ public class ExerciseProfileController {
 
     private final ExerciseProfileService exerciseProfileService;
 
+    @PostMapping
     public ResponseEntity<ExerciseProfileCreateResponse> createExerciseProfile(
             @RequestBody @Valid final ExerciseProfileCreateRequest request,
             @AuthMember final Long memberId
